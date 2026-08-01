cd4cf9a253925f9f81d766b5dc0e4e42af043a31
Dipak Thapa <diipakthapa@gmail.com>
2026-08-01 18:51:06 +0545

    complete day4 foo bar classwork

--- Classword/classwork.js
+++ Classword/classwork.js
@@ -1,11 +1,29 @@
-for (let i=1; i<=100; i++){
-    if (i%3===0 && i%5===0){
-        console.log("foobar");
-    } else if (i%3===0){
-        console.log("foo");
-    } else if (i%5===0){
-        console.log("bar");
-    } else {
-        console.log(i);
+const output = document.getElementById("output");
+
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 === 0 && i % 5 === 0) {
+
+        output.innerHTML += "FOOBAR <br>";
+
     }
-    
+
+    else if (i % 3 === 0) {
+
+        output.innerHTML += "FOO <br>";
+
+    }
+
+    else if (i % 5 === 0) {
+
+        output.innerHTML += "BAR <br>";
+
+    }
+
+    else {
+
+        output.innerHTML += i + "<br>";
+
+    }
+
+}
