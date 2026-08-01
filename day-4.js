54289ce4af46a3e2faf0f6f495938448695562de
Dipak Thapa <diipakthapa@gmail.com>
2026-08-01 18:06:29 +0545

    classwork

---
+++ day-4.js
@@ -0,0 +1,17 @@
+console.log("Day-4")
+
+//Strings
+let str1= "Hello World";
+
+console.log(str1);
+console.log(typeof str1);
+
+//indexing individual characters
+
+const thirdchar = str1[4]
+console.log(thirdchar);
+
+const negativeindex = str1[-1]
+console.log 
+
+
